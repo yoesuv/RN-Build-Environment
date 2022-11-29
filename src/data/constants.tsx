194c50eaf52ce10cb94bab342086d1ef7a3ebd0b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-29 15:54:14 +0700

    :newspaper: add constant

---
+++ src/data/constants.tsx
@@ -0,0 +1,3 @@
+export const BASE_URL_PROD = "https://production.react-native.com";
+export const BASE_URL_STAGING = "https://staging.react-native.com";
+export const BASE_URL_DEV = "https://development.react-native.com";
