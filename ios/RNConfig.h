c0e4b69afc6efd45107fd365929a4ad9fa7a8c87
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-12-01 10:35:01 +0700

    :newspaper: add iOS RNConfig

---
+++ ios/RNConfig.h
@@ -0,0 +1,17 @@
+//
+//  RNConfig.h
+//  Flavor
+//
+//  Created by Yusuf Saifudin on 01/12/22.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNConfig : NSObject<RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
